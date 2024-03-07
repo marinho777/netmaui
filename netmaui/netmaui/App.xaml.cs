dc1fbb3895dd556dcd279623fc544a08f88d5818
marinho777 <danielmarinhom@gmail.com>
2024-03-07 12:20:36 -0300

    chore: initial commit

---
+++ netmaui/netmaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace netmaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ netmaui/netmaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace netmaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
